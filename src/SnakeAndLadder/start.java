5af3238f8d445bf65a9842b190a8aa276d64708a
shreyas329 <sharmayashu1301@gmail.com>
2022-08-17 21:20:50 +0530

    Starting position of game is added.

---
+++ src/SnakeAndLadder/start.java
@@ -0,0 +1,11 @@
+package SnakeAndLadder;
+
+public class start {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Snake and ladder problem...!");
+		int startPosition = 0;
+		System.out.println("Starting position is :"+startPosition);
+	}
+
+}
