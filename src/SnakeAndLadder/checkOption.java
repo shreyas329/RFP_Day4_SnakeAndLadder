bf733ac7607639b5dffbe9953f40f4b8dea9312c
shreyas329 <sharmayashu1301@gmail.com>
2022-08-17 22:43:02 +0530

    No play ladder or snake

---
+++ src/SnakeAndLadder/checkOption.java
@@ -0,0 +1,20 @@
+package SnakeAndLadder;
+import java.util.*;
+public class checkOption {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Snake and ladder problem...!");
+		 
+		 Scanner read = new Scanner(System.in);
+         int position = 0;
+         System.out.println("The position  number is at 0 ");
+         Random random = new Random();
+         int dice1 = random.nextInt(6) + 1;
+         System.out.println("The player rolls dice " + dice1);
+         
+		
+
+	}
+
+}
